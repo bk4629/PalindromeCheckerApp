d22bf722cd519a80c89909e4b788070de227ca8b
bk4629 <bhanu.kreds@gmail.com>
2026-04-08 16:27:39 +0530

    UC10: Loop-based check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,22 +2,27 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    public static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end)
-            return true;
-        if (str.charAt(start) != str.charAt(end))
-            return false;
-        return isPalindrome(str, start + 1, end - 1);
+    public static boolean isPalindrome(String str) {
+        String normalized = str.replaceAll("\\s+", "").toLowerCase();
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Palindrome Checker App");
-        System.out.println("UC9: Recursive Palindrome Checker");
+        System.out.println("UC10: Case-Insensitive & Space-Ignored Palindrome");
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
-        boolean result = isPalindrome(input, 0, input.length() - 1);
-        if (result)
+        if (isPalindrome(input))
             System.out.println("The given string is a palindrome.");
         else
             System.out.println("The given string is not a palindrome.");
