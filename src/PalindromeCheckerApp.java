10fa2ccba375275fa2210cfe91512cdf341a10dd
bk4629 <bhanu.kreds@gmail.com>
2026-04-08 16:27:02 +0530

    UC9: Numeric palindrome support

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,74 +2,22 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    static class Node {
-        char data;
-        Node next;
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    public static Node createLinkedList(String str) {
-        Node head = null, tail = null;
-        for (char ch : str.toCharArray()) {
-            Node newNode = new Node(ch);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-        return head;
-    }
-
-    public static Node reverse(Node head) {
-        Node prev = null;
-        while (head != null) {
-            Node next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
-        }
-        return prev;
-    }
-
-    public static boolean isPalindrome(Node head) {
-        if (head == null || head.next == null)
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end)
             return true;
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data)
-                return false;
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+        return isPalindrome(str, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Palindrome Checker App");
-        System.out.println("UC8: Linked List Based Palindrome Checker");
+        System.out.println("UC9: Recursive Palindrome Checker");
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
-        Node head = createLinkedList(input);
-        if (isPalindrome(head))
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+        if (result)
             System.out.println("The given string is a palindrome.");
         else
             System.out.println("The given string is not a palindrome.");
